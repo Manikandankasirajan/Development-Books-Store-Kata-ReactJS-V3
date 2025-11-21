114dd99f9c40037c190d7e863291ab21e21a0a6a
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-21 21:54:28 +0530

    feat : add feature to show truncated title when book title is more than limit

--- Development-Store/src/components/Product/Product.jsx
+++ Development-Store/src/components/Product/Product.jsx
@@ -7,7 +7,9 @@ const Product = ({ book }) => {
 		<>
 			<section className="p-3 w-68 bg-white shadow-2xl hover:scale-105 transition delay-150 duration-300 ease-in-out cursor-pointer">
 				<img className="w-62 h-72 mb-3" src={bookImg} alt={bookTitle} />
-				<h3 className="mb-1 text-md font-bold text-black/75">{bookTitle}</h3>
+				<h3 className="mb-1 text-md font-bold text-black/75">
+					{bookTitle.length > 30 ? bookTitle.slice(0, 25) + "..." : bookTitle}
+				</h3>
 				<h4 className="text-md font-bold mb-1 text-gray-700">
 					&#x20b9; {bookPrice}
 				</h4>
--- Development-Store/src/components/Product/Product.test.jsx
+++ Development-Store/src/components/Product/Product.test.jsx
@@ -3,31 +3,45 @@ import Product from "./Product";
 import { expect } from "vitest";
 
 describe("test cases for product component", () => {
-	const book = {
-		id: 1,
-		title: "Clean Code",
+	const book1 = {
+		id: 3,
+		title: "Clean Architecture",
 		author: "Robert Martin",
-		releaseYear: 2008,
+		releaseYear: 2017,
 		price: 50,
 		imgSrc: "../public/Kata_DevelopmentBooks_CleanArchitecture.jpeg",
 	};
+	const book2 = {
+		id: 5,
+		title: "Working Effectively With Legacy Code",
+		author: "Michael C. Feathers",
+		releaseYear: 2004,
+		price: 50,
+		imgSrc: "../public/Kata_DevelopmentBooks_Refactoring.jpeg",
+	};
 	it("should render book image,", () => {
-		render(<Product book={book} />);
+		render(<Product book={book1} />);
 		const bookImg = screen.getByRole("img");
-		expect(bookImg).toHaveAttribute("src", book.imgSrc);
+		expect(bookImg).toHaveAttribute("src", book1.imgSrc);
 	});
 	it("should render book title,", () => {
-		render(<Product book={book} />);
+		render(<Product book={book1} />);
 		const bookTitle = screen.getByRole("heading", { level: 3 });
-		expect(bookTitle).toHaveTextContent(book.title);
+		expect(bookTitle).toHaveTextContent(book1.title);
+	});
+	it("should render truncated book title when book title exceeds limit,", () => {
+		const truncatedTitle = book2.title.slice(0, 25) + "...";
+		render(<Product book={book2} />);
+		const bookTitle = screen.getByRole("heading", { level: 3 });
+		expect(bookTitle).toHaveTextContent(truncatedTitle);
 	});
 	it("should render book price,", () => {
-		render(<Product book={book} />);
+		render(<Product book={book1} />);
 		const bookPrice = screen.getByRole("heading", { level: 4 });
-		expect(bookPrice).toHaveTextContent(book.price);
+		expect(bookPrice).toHaveTextContent(book1.price);
 	});
 	it("should render add to cart button,", () => {
-		render(<Product book={book} />);
+		render(<Product book={book1} />);
 		const addToCartBtn = screen.getByRole("button");
 		expect(addToCartBtn).toHaveTextContent(/add to cart/i);
 	});
