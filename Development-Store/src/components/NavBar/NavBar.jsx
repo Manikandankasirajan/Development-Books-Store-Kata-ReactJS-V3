f65f1befa3826f29be862e65c9687514aefdb6f0
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-21 20:26:41 +0530

    feat : add cart button to navbar

--- Development-Store/src/components/NavBar/NavBar.jsx
+++ Development-Store/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaBagShopping } from "react-icons/fa6";
 
 const NavBar = () => {
 	return (
@@ -8,6 +9,11 @@ const NavBar = () => {
 					<h1 className="text-3xl font-bold text-white">
 						Development Books Store
 					</h1>
+					<section className="flex justify-around items-center ml-5">
+						<button className="mr-3 text-3xl text-white font-bold cursor-pointer hover:scale-110 transition delay-150 duration-300 ease-in-out">
+							<FaBagShopping />
+						</button>
+					</section>
 				</nav>
 			</header>
 		</>
--- Development-Store/src/components/NavBar/NavBar.test.jsx
+++ Development-Store/src/components/NavBar/NavBar.test.jsx
@@ -3,9 +3,15 @@ import { expect } from "vitest";
 import NavBar from "./NavBar";
 
 describe("test cases for NavBar component", () => {
-	render(<NavBar />);
 	it("should render heading/logo", () => {
+		render(<NavBar />);
 		const logo = screen.getByRole("heading", { level: 1 });
 		expect(logo).toHaveTextContent(/development books store/i);
+	});
+	it("should render toggle shopping cart button with fa shopping cart icon", () => {
+		render(<NavBar />);
+		const btn = screen.getByRole("button");
+		const shoppingCartIcon = btn.querySelector("svg");
+		expect(shoppingCartIcon).toBeInTheDocument();
 	});
 });
