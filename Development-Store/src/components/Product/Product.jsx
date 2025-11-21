719d341443ae727e51af6665d30df60ee8b46dcc
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-21 21:38:53 +0530

    feat : add feature to show book title in product component

--- Development-Store/src/components/Product/Product.jsx
+++ Development-Store/src/components/Product/Product.jsx
@@ -7,6 +7,7 @@ const Product = ({ book }) => {
 		<>
 			<section className="p-3 w-68 bg-white shadow-2xl hover:scale-105 transition delay-150 duration-300 ease-in-out cursor-pointer">
 				<img className="w-62 h-72 mb-3" src={bookImg} alt={bookTitle} />
+				<h3 className="mb-1 text-md font-bold text-black/75">{bookTitle}</h3>
 			</section>
 		</>
 	);
--- Development-Store/src/components/Product/Product.test.jsx
+++ Development-Store/src/components/Product/Product.test.jsx
@@ -3,17 +3,22 @@ import Product from "./Product";
 import { expect } from "vitest";
 
 describe("test cases for product component", () => {
-	it("should render bookImg", () => {
-		const book = {
-			id: 1,
-			title: "Clean Code",
-			author: "Robert Martin",
-			releaseYear: 2008,
-			price: 50,
-			imgSrc: "../public/Kata_DevelopmentBooks_CleanArchitecture.jpeg",
-		};
+	const book = {
+		id: 1,
+		title: "Clean Code",
+		author: "Robert Martin",
+		releaseYear: 2008,
+		price: 50,
+		imgSrc: "../public/Kata_DevelopmentBooks_CleanArchitecture.jpeg",
+	};
+	it("should render book image,", () => {
 		render(<Product book={book} />);
 		const bookImg = screen.getByRole("img");
 		expect(bookImg).toHaveAttribute("src", book.imgSrc);
+	});
+	it("should render book title,", () => {
+		render(<Product book={book} />);
+		const bookTitle = screen.getByRole("heading", { level: 3 });
+		expect(bookTitle).toHaveTextContent(book.title);
 	});
 });
