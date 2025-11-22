249e57d55db0c14df0d5fad0fa2b019985f9f8f9
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-11-22 10:23:03 +0530

    feat : configure clear cart button with remove all from cart reducer to clear cart

--- Development-Store/src/components/FilledCart/FilledCart.jsx
+++ Development-Store/src/components/FilledCart/FilledCart.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CartItem from "../CartItem/CartItem";
 import CartPriceDetails from "../CartPriceDetails/CartPriceDetails";
+import { removeAllFromCart } from "../../features/cart/cartSlice";
 
 const FilledCart = () => {
 	const cart = useSelector((state) => state.cart.value);
+	const dispatch = useDispatch();
 	return (
 		<>
 			<section className="p-6">
@@ -25,7 +27,9 @@ const FilledCart = () => {
 				</section>
 				<CartPriceDetails />
 				<section className="flex justify-around items-center mt-5">
-					<button className="px-10 py-2 bg-slate-400 font-bold rounded-xl hover:bg-slate-600 hover:cursor-pointer hover:scale-105 transition delay-150 duration-300 ease-in-out">
+					<button
+						className="px-10 py-2 bg-slate-400 font-bold rounded-xl hover:bg-slate-600 hover:cursor-pointer hover:scale-105 transition delay-150 duration-300 ease-in-out"
+						onClick={() => dispatch(removeAllFromCart())}>
 						Clear Cart
 					</button>
 					<button className="px-10 py-2 bg-blue-900 text-white  font-bold  rounded-xl hover:bg-blue-700 hover:cursor-pointer hover:scale-105 transition delay-150 duration-300 ease-in-out">
